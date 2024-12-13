e27de2e5b52aed98ee22958c532733e4480a0cbc
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2024-12-13 02:25:07 +0200

    Update traversals.py

--- traversals.py
+++ traversals.py
@@ -12,12 +12,14 @@ def read_file(filename: str) -> list[tuple]:
 
     :param filename: str, name of .dot file
     :return: list[tuple], list of edges for the given graph
+    >>> read_file('input.dot')
+    [(0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1)]
     """
     with open(filename, "r", encoding="utf-8") as file:
         edges_list = []
         for line in file.readlines()[1:-1]:
             line = line.strip().split("->")
-            edges_list.append((int(line[0].strip()), int(line[1].strip())))
+            edges_list.append((int(line[0].strip()), int(line[1].strip(';'))))
     return edges_list
 
 
@@ -25,6 +27,8 @@ def read_incidence_matrix(filename: str) -> list[list]:
     """
     :param str filename: path to file
     :returns list[list]: the incidence matrix of a given graph
+    >>> read_incidence_matrix('input.dot')
+    [[-1, 0, 1, 1, 0, -1], [0, -1, 0, -1, 1, 1], [1, 1, -1, 0, -1, 0]]
     """
     graph = read_file(filename)
 
@@ -32,7 +36,7 @@ def read_incidence_matrix(filename: str) -> list[list]:
     for v1, v2 in graph:
         all_vertices.add(v1)
         all_vertices.add(v2)
-    matrix = [len(graph) * [0 * (len(graph))] for i in range(1, len(all_vertices) + 1)]
+    matrix = [len(graph) * [0 * (len(graph))] for _ in range(1, len(all_vertices) + 1)]
 
     for i, edge in enumerate(graph):
         if edge[0] == edge[1]:
@@ -41,15 +45,15 @@ def read_incidence_matrix(filename: str) -> list[list]:
             matrix[edge[0] - 1][i] = 1
             matrix[edge[1] - 1][i] = -1
 
-    print(matrix)
+    return matrix
 
 
 def read_adjacency_matrix(filename: str) -> list[list]:
     """
     :param str filename: path to file
     :returns list[list]: the adjacency matrix of a given graph
-    # >>> read_adjacency_matrix('1.dot')
-    # [[0, 1, 0, 0], [0, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]]
+    >>> read_adjacency_matrix('input.dot')
+    [[0, 1, 0], [1, 0, 0], [0, 0, 0]]
     """
     graph = read_file(filename)
     all_vertices = set()
@@ -59,7 +63,7 @@ def read_adjacency_matrix(filename: str) -> list[list]:
 
     matrix = [
         len(all_vertices) * [0 * (len(all_vertices))]
-        for i in range(1, len(all_vertices) + 1)
+        for _ in range(1, len(all_vertices) + 1)
     ]
 
     for i, _ in enumerate(matrix):
@@ -73,8 +77,8 @@ def read_adjacency_dict(filename: str) -> dict[int, list[int]]:
     """
     :param str filename: path to file
     :returns dict: the adjacency dict of a given graph
-    # >>> read_adjacency_dict('1.dot')
-    # {1: {2}, 2: {1, 3}, 3: {2, 4}, 4: {3}}
+    >>> read_adjacency_dict('input.dot')
+    {0: {1, 2}, 1: {0, 2}, 2: {0, 1}}
     """
     edges_list = read_file(filename)
 
@@ -109,7 +113,7 @@ def iterative_adjacency_dict_dfs(graph: dict[int, list[int]], start: int) -> lis
         visited[curr_vertice] = True
         if curr_vertice not in component:
             component.append(curr_vertice)
-        for neighbour in reversed(graph.get(curr_vertice)):
+        for neighbour in reversed(list(graph.get(curr_vertice))):
             if visited[neighbour] is False:
                 stack.append(neighbour)
 
@@ -138,16 +142,6 @@ def iterative_adjacency_matrix_dfs(graph: list[list], start: int) -> list[int]:
                 if graph[curr_vertice][i] == 1 and i not in visited:
                     vertices.append(i)
     return res
-
-    # vertices = [start]
-    # res = []
-    # while vertices:
-    #     curr_vertice = vertices.pop()
-    #     res.append(curr_vertice)
-    #     for i in range(len(graph[curr_vertice]) - 1, -1, -1):
-    #         if graph[curr_vertice][i] == 1:
-    #             vertices.append(i)
-    # return res
 
 
 def recursive_adjacency_dict_dfs(graph: dict[int, list[int]], start: int) -> list[int]:
@@ -309,7 +303,8 @@ def adjacency_dict_radius(graph: dict[int : list[int]]) -> int:
     return radius
 
 
-def find_function_runtime(func, graph: list[list[int]]) -> float:
+def find_function_runtime(func, path = None, matrix = None,\
+    start_point = 0, dictionary = None) -> float:
     """
     Returns the runtime of the function
 
@@ -317,14 +312,23 @@ def find_function_runtime(func, graph: list[list[int]]) -> float:
     :param graph: list, The graph that function will work with
     :return: float, The runtime of the function
     """
-    begin = time.time()
-    func(graph)
+    begin = time.perf_counter()
+    if path is not None:
+        func(path)
+    elif matrix is not None:
+        func(matrix, start_point)
+    elif dictionary is not None:
+        func(dictionary, start_point)
     time.sleep(1)
-    end = time.time()
+    end = time.perf_counter()
     return end - begin
+
+print(find_function_runtime(read_incidence_matrix, path='input.dot'))
+print(find_function_runtime(read_adjacency_dict, path='input.dot'))
+print(find_function_runtime(read_adjacency_matrix, path='input.dot'))
+
 
 
 if __name__ == "__main__":
     import doctest
-
     print(doctest.testmod())
